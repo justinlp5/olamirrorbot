4e3a7c0cb57a82c1d40c24ad165c435aa104a96a
justinlp5 <85289106+justinlp5@users.noreply.github.com>
2021-10-06 03:28:16 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -11,62 +11,122 @@ import string
 
 
 def cloneNode(update, context):
-    args = update.message.text.split(" ", maxsplit=1)
-    if len(args) > 1:
-        link = args[1]
-        gd = gdriveTools.GoogleDriveHelper()
-        res, size, name, files = gd.clonehelper(link)
-        if res != "":
-            sendMessage(res, context.bot, update)
-            return
-        if STOP_DUPLICATE:
-            LOGGER.info('Checking File/Folder if already in Drive...')
-            smsg, button = gd.drive_list(name, True)
-            if smsg:
-                msg3 = "File/Folder is already available in Drive.\nHere are the search results:"
-                sendMarkup(msg3, context.bot, update, button)
+    if ',' in link:
+        args = update.message.text.split(" ")
+        for x in args:
+            if "google" in x:
+                x = x.replace(",", "")
+            link = x
+            gd = gdriveTools.GoogleDriveHelper()
+            res, size, name, files = gd.clonehelper(link)
+            if res != "":
+                sendMessage(res, context.bot, update)
                 return
-        if CLONE_LIMIT is not None:
-            result = check_limit(size, CLONE_LIMIT)
-            if result:
-                msg2 = f'Failed, Clone limit is {CLONE_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}.'
-                sendMessage(msg2, context.bot, update)
+            if STOP_DUPLICATE:
+                LOGGER.info('Checking File/Folder if already in Drive...')
+                smsg, button = gd.drive_list(name, True)
+                if smsg:
+                    msg3 = "File/Folder is already available in Drive.\nHere are the search results:"
+                    sendMarkup(msg3, context.bot, update, button)
+                    return
+            if CLONE_LIMIT is not None:
+                result = check_limit(size, CLONE_LIMIT)
+                if result:
+                    msg2 = f'Failed, Clone limit is {CLONE_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}.'
+                    sendMessage(msg2, context.bot, update)
+                    return
+            if files < 15:
+                msg = sendMessage(f"Cloning: <code>{link}</code>", context.bot, update)
+                result, button = gd.clone(link)
+                deleteMessage(context.bot, msg)
+            else:
+                drive = gdriveTools.GoogleDriveHelper(name)
+                gid = ''.join(random.SystemRandom().choices(string.ascii_letters + string.digits, k=12))
+                clone_status = CloneStatus(drive, size, update, gid)
+                with download_dict_lock:
+                    download_dict[update.message.message_id] = clone_status
+                sendStatusMessage(update, context.bot)
+                result, button = drive.clone(link)
+                with download_dict_lock:
+                    del download_dict[update.message.message_id]
+                    count = len(download_dict)
+                try:
+                    if count == 0:
+                        Interval[0].cancel()
+                        del Interval[0]
+                        delete_all_messages()
+                    else:
+                        update_all_messages()
+                except IndexError:
+                    pass
+            if update.message.from_user.username:
+                uname = f'@{update.message.from_user.username}'
+            else:
+                uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+            if uname is not None:
+                cc = f'\n\ncc: {uname}'
+                men = f'{uname} '
+            if button in ["cancelled", ""]:
+                sendMessage(men + result, context.bot, update)
+            else:
+                sendMarkup(result + cc, context.bot, update, button)
+    else:
+        args = update.message.text.split(" ", maxsplit=1) 
+        if len(args) > 1:
+            link = args[1]
+            gd = gdriveTools.GoogleDriveHelper()
+            res, size, name, files = gd.clonehelper(link)
+            if res != "":
+                sendMessage(res, context.bot, update)
                 return
-        if files < 15:
-            msg = sendMessage(f"Cloning: <code>{link}</code>", context.bot, update)
-            result, button = gd.clone(link)
-            deleteMessage(context.bot, msg)
-        else:
-            drive = gdriveTools.GoogleDriveHelper(name)
-            gid = ''.join(random.SystemRandom().choices(string.ascii_letters + string.digits, k=12))
-            clone_status = CloneStatus(drive, size, update, gid)
-            with download_dict_lock:
-                download_dict[update.message.message_id] = clone_status
-            sendStatusMessage(update, context.bot)
-            result, button = drive.clone(link)
-            with download_dict_lock:
-                del download_dict[update.message.message_id]
-                count = len(download_dict)
-            try:
-                if count == 0:
-                    Interval[0].cancel()
-                    del Interval[0]
-                    delete_all_messages()
-                else:
-                    update_all_messages()
-            except IndexError:
-                pass
-        if update.message.from_user.username:
-            uname = f'@{update.message.from_user.username}'
-        else:
-            uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
-        if uname is not None:
-            cc = f'\n\ncc: {uname}'
-            men = f'{uname} '
-        if button in ["cancelled", ""]:
-            sendMessage(men + result, context.bot, update)
-        else:
-            sendMarkup(result + cc, context.bot, update, button)
+            if STOP_DUPLICATE:
+                LOGGER.info('Checking File/Folder if already in Drive...')
+                smsg, button = gd.drive_list(name, True)
+                if smsg:
+                    msg3 = "File/Folder is already available in Drive.\nHere are the search results:"
+                    sendMarkup(msg3, context.bot, update, button)
+                    return
+            if CLONE_LIMIT is not None:
+                result = check_limit(size, CLONE_LIMIT)
+                if result:
+                    msg2 = f'Failed, Clone limit is {CLONE_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}.'
+                    sendMessage(msg2, context.bot, update)
+                    return
+            if files < 15:
+                msg = sendMessage(f"Cloning: <code>{link}</code>", context.bot, update)
+                result, button = gd.clone(link)
+                deleteMessage(context.bot, msg)
+            else:
+                drive = gdriveTools.GoogleDriveHelper(name)
+                gid = ''.join(random.SystemRandom().choices(string.ascii_letters + string.digits, k=12))
+                clone_status = CloneStatus(drive, size, update, gid)
+                with download_dict_lock:
+                    download_dict[update.message.message_id] = clone_status
+                sendStatusMessage(update, context.bot)
+                result, button = drive.clone(link)
+                with download_dict_lock:
+                    del download_dict[update.message.message_id]
+                    count = len(download_dict)
+                try:
+                    if count == 0:
+                        Interval[0].cancel()
+                        del Interval[0]
+                        delete_all_messages()
+                    else:
+                        update_all_messages()
+                except IndexError:
+                    pass
+            if update.message.from_user.username:
+                uname = f'@{update.message.from_user.username}'
+            else:
+                uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+            if uname is not None:
+                cc = f'\n\ncc: {uname}'
+                men = f'{uname} '
+            if button in ["cancelled", ""]:
+                sendMessage(men + result, context.bot, update)
+            else:
+                sendMarkup(result + cc, context.bot, update, button)
     else:
         sendMessage('Provide G-Drive Shareable Link to Clone.', context.bot, update)
 
